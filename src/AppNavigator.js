6b2d1ad8f311a5d42505ca1bb26133bf6f7bbee4
032AbhishekChoksi <19bmiit032@gmail.com>
2023-05-24 23:14:52 +0530

    Login with Firebase

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -4,6 +4,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Splash from './screens/Splash';
 import Login from './screens/Login';
+import Dashboard from './screens/Dashboard';
 
 // create a component
 const Stack = createNativeStackNavigator();
@@ -19,6 +20,11 @@ const AppNavigator = () => {
         <Stack.Screen
           name="Login"
           component={Login}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Dashboard"
+          component={Dashboard}
           options={{headerShown: false}}
         />
       </Stack.Navigator>
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,107 @@
 //import liraries
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
+// create a component
 const Login = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // useEffect(() => {
+  //   // firestore()
+  //   //   .collection('admin')
+  //   //   .add({email: 'admin@gmail.com', password: 'admin@1234'})
+  //   //   .then(() => {
+  //   //     console.log('admin added!');
+  //   //   });
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
+  const adminLogin = async () => {
+    let users = await firestore().collection('admin').get();
+    users = users.docs[0]._data;
+    // console.log(users.docs[0]._data);
+    // console.log(users.docs[0]._data.email);
+    if (email === users.email && password === users.password) {
+      navigation.navigate('Dashboard');
+    } else {
+      alert('wrong email or password!');
+    }
+  };
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Admin Login</Text>
+      <TextInput
+        style={styles.inputStyle}
+        placeholder={'Enter Email ID'}
+        value={email}
+        onChangeText={txt => setEmail(txt)}
+      />
+      <TextInput
+        style={styles.inputStyle}
+        placeholder={'Enter Password'}
+        value={password}
+        onChangeText={txt => setPassword(txt)}
+      />
+      <TouchableOpacity
+        style={styles.loginBtn}
+        onPress={() => {
+          if (email !== '' && password !== '') {
+            adminLogin();
+          } else {
+            alert('Please Enter All Data');
+          }
+        }}>
+        <Text style={styles.btnText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '800',
+    color: '#000',
+    marginTop: 100,
+    alignSelf: 'center',
+  },
+  inputStyle: {
+    paddingLeft: 20,
+    height: 50,
+    alignSelf: 'center',
+    marginTop: 30,
+    borderWidth: 0.5,
+    borderRadius: 10,
+    width: '90%',
+  },
+  loginBtn: {
+    backgroundColor: 'orange',
+    width: '90%',
+    height: 50,
+    alignSelf: 'center',
+    borderRadius: 10,
+    marginTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btnText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#000',
+  },
+});
+
+//make this component available to the app
 export default Login;
