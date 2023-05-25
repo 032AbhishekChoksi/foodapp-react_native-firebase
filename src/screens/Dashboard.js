c4b5a752bd6a719c2d3a6b3e61ebd8a9861787ca
032AbhishekChoksi <19bmiit032@gmail.com>
2023-05-25 10:31:36 +0530

    create custom bottom tab navigation

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,12 +1,97 @@
+/* eslint-disable react-native/no-inline-styles */
 //import liraries
-import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import Items from '../tabs/Items';
+import Transactions from '../tabs/Transactions';
+import Add from '../tabs/Add';
+import Orders from '../tabs/Orders';
+import Notifications from '../tabs/Notifications';
 
 // create a component
 const Dashboard = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Dashboard</Text>
+      {selectedTab === 0 ? (
+        <Items />
+      ) : selectedTab === 1 ? (
+        <Transactions />
+      ) : selectedTab === 2 ? (
+        <Add />
+      ) : selectedTab === 3 ? (
+        <Orders />
+      ) : (
+        <Notifications />
+      )}
+      <View style={styles.bottomView}>
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(0);
+          }}>
+          <Image
+            source={require('../images/items.png')}
+            style={[
+              styles.bottomTabImg,
+              {tintColor: selectedTab === 0 ? 'red' : 'black'},
+            ]}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(1);
+          }}>
+          <Image
+            source={require('../images/transaction.png')}
+            style={[
+              styles.bottomTabImg,
+              {tintColor: selectedTab === 1 ? 'red' : 'black'},
+            ]}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(2);
+          }}>
+          <Image
+            source={require('../images/add.png')}
+            style={[
+              styles.bottomTabImg,
+              {width: 35, height: 35},
+              {tintColor: selectedTab === 2 ? 'red' : 'black'},
+            ]}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(3);
+          }}>
+          <Image
+            source={require('../images/orders.png')}
+            style={[
+              styles.bottomTabImg,
+              {tintColor: selectedTab === 3 ? 'red' : 'black'},
+            ]}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.bottomTab}
+          onPress={() => {
+            setSelectedTab(4);
+          }}>
+          <Image
+            source={require('../images/notification.png')}
+            style={[
+              styles.bottomTabImg,
+              {tintColor: selectedTab === 4 ? 'red' : 'black'},
+            ]}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -15,9 +100,26 @@ const Dashboard = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  bottomView: {
+    width: '100%',
+    height: 60,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: '#fff',
+  },
+  bottomTab: {
+    height: '100%',
+    width: '20%',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#2c3e50',
+  },
+  bottomTabImg: {
+    width: 24,
+    height: 24,
   },
 });
 
